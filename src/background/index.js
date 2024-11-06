7ed99cf2c446b80bc45af36c290ae965c0ecca86
thomas <t.feichtinger@pm.me>
2024-11-06 07:13:54 +0100

    sidepanel now sorts menus by levensthein distance to selected input

--- src/background/index.js
+++ src/background/index.js
@@ -1,93 +1,14 @@
-import { faker, generateMersenne32Randomizer } from '@faker-js/faker'
-
-const contextMenu = {
-  title: 'Fake data...',
-  menues: {
-    company: {
-      title: 'Company',
-      menues: {
-        fullName: {
-          title: 'Full Name',
-          generator: faker.company.name,
-        },
-      },
-    },
-    person: {
-      title: 'Person',
-      menues: {
-        fullName: {
-          title: 'Full Name',
-          generator: faker.person.fullName,
-        },
-      },
-    },
-    address: {
-      title: 'Address',
-      menues: {
-        street: {
-          title: 'Street',
-          generator: faker.location.street,
-        },
-        city: {
-          title: 'City',
-          generator: faker.location.city,
-        },
-      },
-    },
-    finance: {
-      title: 'Finance',
-      menues: {
-        iban: {
-          title: 'IBAN (AT)',
-          generator: faker.finance.iban,
-          config: { formatted: true, countryCode: 'AT' },
-        },
-      },
-    },
-    internet: {
-      title: 'Internet',
-      menues: {
-        email: {
-          title: 'Email',
-          generator: faker.internet.email,
-        },
-        url: {
-          title: 'URL',
-          generator: faker.internet.url,
-        },
-      },
-    },
-    number: {
-      title: 'Numbers',
-      menues: {
-        shortNumber: {
-          title: '1000 <= x <= 10000',
-          generator: faker.number.int,
-          config: {min: 1000, max: 10000}
-        }
-      }
-    },
-    loremIpsum: {
-      title: 'Lorem Ipsum',
-      menues: {
-        loremShort: {
-          title: 'Text',
-          generator: faker.lorem.text,
-        },
-      },
-    },
-  },
-}
+import { menues } from '../menueDefinition.js'
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     id: 'root',
-    title: contextMenu.title,
+    title: 'Random data...',
     contexts: ['editable'],
   })
 
-  Object.keys(contextMenu.menues).forEach((parentKey) => {
-    const parent = contextMenu.menues[parentKey]
+  Object.keys(menues).forEach((parentKey) => {
+    const parent = menues[parentKey]
     chrome.contextMenus.create({
       id: parentKey,
       parentId: 'root',
@@ -109,7 +30,7 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   const [parentKey, childKey] = info.menuItemId.split('-')
-  const menues = contextMenu.menues
+  const menues = menues
   const generator = menues[parentKey].menues[childKey].generator
   const config = menues[parentKey].menues[childKey].config
 
@@ -121,5 +42,9 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
 })
 
 function runScript(info, tab, value) {
-  chrome.tabs.sendMessage(tab.id, { type: 'fillFromContextMenu', value: value }, { frameId: info.frameId })
+  chrome.tabs.sendMessage(
+    tab.id,
+    { type: 'fillFromContextMenu', value: value },
+    { frameId: info.frameId },
+  )
 }
---
+++ src/menueDefinition.js
@@ -0,0 +1,77 @@
+import { faker } from '@faker-js/faker'
+
+export const menues = {
+  address: {
+    title: 'Address',
+    menues: {
+      city: {
+        title: 'City',
+        generator: faker.location.city,
+      },
+      street: {
+        title: 'Street',
+        generator: faker.location.street,
+      },
+    },
+  },
+  company: {
+    title: 'Company',
+    menues: {
+      fullName: {
+        title: 'Full Name',
+        generator: faker.company.name,
+      },
+    },
+  },
+  finance: {
+    title: 'Finance',
+    menues: {
+      iban: {
+        title: 'IBAN (AT)',
+        generator: faker.finance.iban,
+        config: { formatted: true, countryCode: 'AT' },
+      },
+    },
+  },
+  internet: {
+    title: 'Internet',
+    menues: {
+      email: {
+        title: 'Email',
+        generator: faker.internet.email,
+      },
+      url: {
+        title: 'URL',
+        generator: faker.internet.url,
+      },
+    },
+  },
+  number: {
+    title: 'Numbers',
+    menues: {
+      shortNumber: {
+        title: '1000 <= x <= 10000',
+        generator: faker.number.int,
+        config: { min: 1000, max: 10000 },
+      },
+    },
+  },
+  loremIpsum: {
+    title: 'Lorem Ipsum',
+    menues: {
+      loremShort: {
+        title: 'Text',
+        generator: faker.lorem.text,
+      },
+    },
+  },
+  person: {
+    title: 'Person',
+    menues: {
+      fullName: {
+        title: 'Full Name',
+        generator: faker.person.fullName,
+      },
+    },
+  },
+}
